73c97cecf3718329b2ca9d42799850984fd2d90c
alireza200411 <alirezamoradi292048@gmail.com>
2024-12-17 18:46:49 +0330

    add model product

--- bootshop/settings.py
+++ bootshop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from os import path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'product'
+    'product',
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    path.join(BASE_DIR, 'assets')
+]
+MEDIA_ROOT = path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- product/models.py
+++ product/models.py
@@ -1,7 +1,24 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.utils.text import slugify
 
 
 class Product(models.Model):
     brand = models.CharField(max_length=60)
     model = models.CharField(max_length=70)
+    date = models.DateField(auto_now_add=True)
+    price = models.DecimalField(max_digits=12, decimal_places=3, validators=[MinValueValidator(0)])
+    slug = models.SlugField()
+    stock = models.IntegerField()
+    view = models.IntegerField()
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.model)
+
+
+class Image(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='media/product')
+
+    def __str__(self):
+        return self.product.model
